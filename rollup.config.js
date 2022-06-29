026dc5d53c484371e23d90eec494572d65202e0f
David Pyke <Shoelace@users.noreply.github.com>
2022-06-30 09:30:59 +1000

    dependencies updates
    update dependencies and minor edits to compile with latest verions.
    
    also generate non-minified umd files and source maps

--- rollup.config.js
+++ rollup.config.js
@@ -3,35 +3,56 @@
  * Contract: i@hust.cc
  */
 
-import { uglify } from 'rollup-plugin-uglify';
-import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript';
+
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 module.exports = [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/timeago.min.js',
-      name: 'timeago',
-      format: 'umd',
-    },
+    output: [
+      {
+        file: 'dist/timeago.js',
+        name: 'timeago',
+        format: 'umd',
+      },
+      {
+        sourcemap: true,
+        file: 'dist/timeago.min.js',
+        name: 'timeago',
+        format: 'umd',
+        plugins: [
+          terser(),
+        ],
+      }
+    ]
+    ,
     plugins: [
       resolve(),
       typescript(),
-      uglify(),
+      terser(),
     ],
   },
   {
     input: 'src/full.ts',
-    output: {
+    output: [{
+      file: 'dist/timeago.full.js',
+      name: 'timeago',
+      format: 'umd',
+    }, {
       file: 'dist/timeago.full.min.js',
       name: 'timeago',
       format: 'umd',
-    },
+      sourcemap: true,
+      plugins: [
+        terser(),
+      ],
+    }],
     plugins: [
       resolve(),
       typescript(),
-      uglify(),
+      terser(),
     ],
   },
 ];
